17b96effdfc908b8e8f3ddd97e3a22696b9f65f1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-06 09:18:42 -0300

    tudo arrumado sem dar erros

--- pythonJoaoP/Entrega modulo 1 Joao Pedro/ativModulo1.py
+++ pythonJoaoP/Entrega modulo 1 Joao Pedro/ativModulo1.py
@@ -1,36 +1,77 @@
+import os
+
+
 usuarios = []
-ui = 0
+i=0
+home = 0
+home_opcao = 0
 
-def novo_usuario(self, nome,senha):
-    self.nome = str(input("Digite o nome do novo usuário: "))
-    self.senha = str(input("Digite a senha do novo usuário: "))
-    usuarios[ui] = {self.nome: self.senha}
-    ui += 1
-    print("Usuário criado com sucesso!")
-
-def login():
-    try:
-        print("Digite o que deseja fazer!")
-        print("1 - entrar")
-        print("2 - criar novo usuario")
-        print("3 - sair\n")
-        opcao = int(input())
-        if opcao >3 or opcao <1:
-            raise ValueError
-        elif opcao == 1:
-            pass
-        elif opcao == 2:
-            novo_usuario()
-        elif opcao == 3:
-            print("Saindo...")
-            exit()
-
-    except ValueError:
-        print("Valor inválido!\n Digite os números comforme cada opção\n digite 1 para entrar, 2 para criar novo usuario e 3 para sair")
+def Home():
+    global home_opcao
+    print(f"-- Bem vindo ao sistema de avaliar filmes {nome}! --\n")
+    print("1 - adicionar Filme")
+    print("2 - avaliar e ver filmes")
+    print("3 - ver usuários")
+    print("4 - logout")
+    print("5 - sair do sistema\n")
+    home_opcao = int(input())
 
 while True:
-    login()
+    try: 
+        if home != 1:
+            print("| Sistema de avaliar filmes |\n")
+            print("Digite o que deseja fazer!")
+            print("1 - entrar")
+            print("2 - criar novo usuario")
+            print("3 - sair\n")
+            opcao = int(input())
+            if opcao >3 or opcao <1:
+                raise ValueError
+            elif opcao == 1:
+                if usuarios == []:
+                    print("Nenhum usuário cadastrado! Crie um novo usuário.\n")
+                    continue
+                nome = str(input("Digíte o seu nome :\n")).strip()
+                senha = str(input("Digíte a sua senha :\n")).strip()
+                for usuario in usuarios:
+                    if usuario['nome'] == nome and usuario['senha'] == senha:
+                        os.system('cls' if os.name == 'nt' else 'clear') # comando para limpar a tela
+                        print("Login realizado com sucesso!")
+                        home = 1
+                    else:
+                        i += 1
+                        if i == len(usuarios):
+                            print("Usuário ou senha incorretos!")
+                            i=0
+            
+            elif opcao == 2:
+                nome = str(input("Digíte o nome do novo usuário:\n")).strip()
+                senha = str(input("Digíte a senha do novo usuário:\n")).strip()
+                usuarios.append({'nome': nome, 'senha': senha})
+                os.system('cls' if os.name == 'nt' else 'clear') # comando para limpar a tela
+                print("Usuário CASDASTRADO com sucesso!")
+    
+            elif opcao == 3:
+                print("Saindo...")
+                exit()
+    except ValueError:
+        print("Valor inválido! Digíte a opção corretamente\n")
     try:
-        print(usuarios[0])
-    except IndexError:
-        pass
+        while home == 1:
+            Home()
+            if home_opcao == 1:
+                pass
+            if home_opcao == 2:
+                pass
+            if home_opcao == 3:
+                pass
+            elif home_opcao == 4:
+                print("Fazendo logout...\n")
+                home = 0
+            elif home_opcao == 5:
+                print("Saindo...")
+                exit()
+            elif home_opcao >5 or home_opcao <1:
+                raise ValueError
+    except ValueError:
+        print("Opção inválido! Digíte a opção corretamente\n")
