122a34f363325a3fdfc6adb845603df5d62401de
joaopedrocerqueira1305-ops <joaopedrocerqueira1305@gmail.com>
2025-11-29 12:11:43 -0300

    Delete capacitaPhyton-main/pythonJoaoP directory

--- capacitaPhyton-main/pythonJoaoP/aula3/Animal.py
+++
@@ -1,7 +0,0 @@
-class Animal:
-    def __init__(self, nome,sexo):
-        self.nome = nome
-        self.sexo = sexo
-
-    def emitir_som():
-        pass
--- capacitaPhyton-main/pythonJoaoP/aula4/veiculo/Moto.py
+++
@@ -1,8 +0,0 @@
-from Veiculo import Veiculo
-
-class Moto(Veiculo):
-    def __init__(self, nome):
-        super().__init__(nome)
-
-    def dirigir(self):
-        return (f"A moto está fazendo uma curva.")
