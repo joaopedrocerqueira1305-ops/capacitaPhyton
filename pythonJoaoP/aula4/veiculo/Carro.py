11ba51c26461d66b110c1cb1bae08e98ca54f0c4
joaopedrocerqueira1305-ops <joaopedrocerqueira1305@gmail.com>
2025-12-06 12:00:55 -0300

    Delete pythonJoaoP directory

--- pythonJoaoP/aula4/veiculo/Carro.py
+++
@@ -1,5 +0,0 @@
-from Veiculo import Veiculo
-
-class Carro(Veiculo):
-    def dirigir(self):
-        return (f"O carro está acelerando na estrada.")
--- pythonJoaoP/aula5/exercicio3/Main.py
+++
@@ -1,6 +0,0 @@
-from Pedido import Pedido
-
-
-p1 = Pedido("arroz",3)
-p1.definirQuantidade(3)
-print(p1.resumo())
