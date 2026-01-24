edd6f129da014df651606d11cd64cc40d0c0c13c
joaopedrocerqueira1305-ops <joaopedrocerqueira1305@gmail.com>
2026-01-24 11:32:29 -0300

    Delete pythonJoaoP directory

--- pythonJoaoP/aula4/veiculo/Caminhao.py
+++
@@ -1,9 +0,0 @@
-from Veiculo import Veiculo
-
-
-class Caminhao(Veiculo):
-    def __init__(self, nome):
-        super().__init__(nome)
-
-    def dirigir(self):
-        return (f"O caminhão está transportando carga.")
